d6c8f6c167891c776757ccc3cd2608f4e4fc1d53
Khaled Abdulhamid <khaledraedabdulhamed101@hotmail.com>
2023-06-20 10:37:33 -0400

    adding a reducer function instead of context for the usercontext

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,5 +1,5 @@
 import DirectoryItem from "../directory-item/directory-item.component"
-import './directory.style.scss'
+import {DirectoryContainer} from './directory.style'
 const categories = [
     {
       "id": 1,
@@ -38,11 +38,11 @@ const categories = [
   ]
 function Directory (){
     return(
-    <div className="directory-container">
+    <DirectoryContainer>
     {categories.map((category) => (
      <DirectoryItem key={category.id} category={category} />
 ))}
-</div>
+</DirectoryContainer>
 )
 }
 
--- src/context/user.context.jsx
+++ src/context/user.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState, useEffect, useReducer } from "react";
 import { onAuthStateChangedListener, createUserDocumentFromAuth } from "../utils/firebase/firebase.utils";
 
 export const userContext = createContext({
@@ -6,9 +6,48 @@ export const userContext = createContext({
     setCurrentUser: () => null,
 })
 
+export const USER_ACTION_TYPES = {
+
+    SET_CURRENT_USER: 'SET_CURRENT_USER'
+}
+
+const userReducer = (state, action) =>{
+    const {type, payload} = action
+    console.log(action)
+    console.log("dispatched")
+
+    switch(type){
+        case USER_ACTION_TYPES.SET_CURRENT_USER:
+            return {
+                ...state,
+                currentUser: payload
+            }
+            default:
+                throw new Error(`unhandled type ${type} in userReducer`)
+    }
+
+}
+    const INITIAL_STATE = {
+        currentUser: null
+    }
+
+
+
+
+
+
 export const UserProvider = ({children}) => {
-    const [currentUser, setCurrentUser] = useState(null);
-    const value = {currentUser, setCurrentUser};
+    // const [currentUser, setCurrentUser] = useState(null);
+
+    const [state, dispatch] = useReducer(userReducer,INITIAL_STATE)
+    const {currentUser} = state
+    console.log(currentUser)
+
+    const setCurrentUser = (user) =>{
+        dispatch({type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user})
+    }
+
+    const value = {currentUser,setCurrentUser}
 
     useEffect(()=>{
     const  unsubscribe =  onAuthStateChangedListener((user)=>{
